7e226feb6a389e7e883ee8c977398700b7ac64da
Carlsilber <carl.silber74@gmail.com>
2018-02-22 13:43:37 +0200

    Added Publisher stuff

---
+++ src/main/java/com/carlsilber/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.carlsilber.spring5webapp.repositories;
+
+import com.carlsilber.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
